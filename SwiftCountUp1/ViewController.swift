e8c1aa02245c521986207428486bdc32d01f8b80
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-27 09:10:49 +0900

    加減した際の処理などを実装

--- SwiftCountUp1/ViewController.swift
+++ SwiftCountUp1/ViewController.swift
@@ -11,11 +11,49 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var countLabel: UILabel!
   
+  var count = 0
+  
+  // Viewが呼ばれたとき
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    countLabel.text = "1"
   }
+  
+  // プラスボタン
+  @IBAction func plus(_ sender: Any) {
+    
+    count = count + 1
+    
+    countLabel.text = String(count)
+    
+    // 色を黄色くする
+    if count >= 10 {
+      changeTextColor()
+    }
+    
+  }
+  
+  func changeTextColor() {
+    countLabel.textColor = .yellow
+  }
+  
+  func resetColor() {
+    countLabel.textColor = .white
+  }
+  
+  // マイナスボタン
+  @IBAction func minus(_ sender: Any) {
+    
+    count = count - 1
+    
+    countLabel.text = String(count)
 
-
+    // 色を白くする
+    if count <= 10 {
+      resetColor()
+      
+    }
+  }
 }
 
